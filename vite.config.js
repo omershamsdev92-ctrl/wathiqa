54271038b1761e4c7595a6e8230ce6df29ede00b
omershamsdev92-ctrl <omershamsdev92@gmail.com>
2026-01-17 16:45:15 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                viewer: resolve(__dirname, 'viewer.html'),
+                admin: resolve(__dirname, 'admin.html'),
+            },
+        },
+    },
+});
